0b53991d4fd3fd0e75aee5fa9cea7a17497bee6e
qiudfvs <103111469+qiudfvs@users.noreply.github.com>
2023-03-07 03:23:19 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=1550fdcf-3b25-42a8-af6c-60975792530a
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=www.ruudbergamin.info
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
